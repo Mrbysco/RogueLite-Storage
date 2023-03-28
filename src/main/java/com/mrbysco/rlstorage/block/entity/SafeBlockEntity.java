4953d318ff519e990ab7f32b048879df0e10452e
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-28 17:14:22 +0200

    Update to 1.19.2

--- src/main/java/com/mrbysco/rlstorage/block/entity/SafeBlockEntity.java
+++ src/main/java/com/mrbysco/rlstorage/block/entity/SafeBlockEntity.java
@@ -5,22 +5,20 @@ import com.mrbysco.rlstorage.storage.SafeData;
 import com.mrbysco.rlstorage.storage.SafeInventory;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.UUID;
 
 public class SafeBlockEntity extends BlockEntity {
 	public SafeBlockEntity(BlockPos pos, BlockState state) {
-		super((BlockEntityType) RLRegistry.SAFE_BLOCK_ENTITY.get(), pos, state);
+		super(RLRegistry.SAFE_BLOCK_ENTITY.get(), pos, state);
 	}
 
 	public Component getDisplayName() {
-		return new TranslatableComponent("roguelitestorage.container.safe");
+		return Component.translatable("roguelitestorage.container.safe");
 	}
 
 	public boolean stillValid(Player player) {
@@ -28,9 +26,9 @@ public class SafeBlockEntity extends BlockEntity {
 			return false;
 		} else {
 			return !(player.distanceToSqr(
-					(double)this.worldPosition.getX() + 0.5D,
-					(double)this.worldPosition.getY() + 0.5D,
-					(double)this.worldPosition.getZ() + 0.5D) > 64.0D);
+					(double) this.worldPosition.getX() + 0.5D,
+					(double) this.worldPosition.getY() + 0.5D,
+					(double) this.worldPosition.getZ() + 0.5D) > 64.0D);
 		}
 	}
 
--- src/main/java/com/mrbysco/rlstorage/registry/RLRegistry.java
+++ src/main/java/com/mrbysco/rlstorage/registry/RLRegistry.java
@@ -1,6 +1,6 @@
 package com.mrbysco.rlstorage.registry;
 
-import com.mojang.datafixers.types.Type;
+import com.mrbysco.rlstorage.RogueLiteStorage;
 import com.mrbysco.rlstorage.block.SafeBlock;
 import com.mrbysco.rlstorage.block.entity.SafeBlockEntity;
 import net.minecraft.world.item.BlockItem;
@@ -8,6 +8,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.BlockEntityType.Builder;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.material.Material;
@@ -17,14 +18,14 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class RLRegistry {
-	public static final DeferredRegister BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "roguelitestorage");
-	public static final DeferredRegister ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "roguelitestorage");
-	public static final DeferredRegister BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, "roguelitestorage");
-	public static final RegistryObject SAFE = BLOCKS.register("safe", () ->
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RogueLiteStorage.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RogueLiteStorage.MOD_ID);
+	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, RogueLiteStorage.MOD_ID);
+	public static final RegistryObject<Block> SAFE = BLOCKS.register("safe", () ->
 			new SafeBlock(Properties.of(Material.HEAVY_METAL, MaterialColor.METAL).requiresCorrectToolForDrops()
 					.strength(5.0F, 1200.0F).sound(SoundType.ANVIL)));
-	public static final RegistryObject SAFE_BLOCK_ENTITY = BLOCK_ENTITIES.register("safe", () ->
-			Builder.of(SafeBlockEntity::new, new Block[]{(Block) SAFE.get()}).build((Type) null));
-	public static final RegistryObject SAFE_ITEM = ITEMS.register("safe", () ->
-			new BlockItem((Block) SAFE.get(), (new Item.Properties()).tab(CreativeModeTab.TAB_MISC)));
+	public static final RegistryObject<BlockEntityType<?>> SAFE_BLOCK_ENTITY = BLOCK_ENTITIES.register("safe", () ->
+			Builder.of(SafeBlockEntity::new, new Block[]{SAFE.get()}).build(null));
+	public static final RegistryObject<Item> SAFE_ITEM = ITEMS.register("safe", () ->
+			new BlockItem(SAFE.get(), (new Item.Properties()).tab(CreativeModeTab.TAB_MISC)));
 }
